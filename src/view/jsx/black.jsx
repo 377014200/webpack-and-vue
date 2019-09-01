aefbc2126eb2873e300897f9b6f473a457f51195
DBL <377014200@qq.com>
2019-09-02 02:28:58 +0800

    package-lock.json

---
+++ src/view/jsx/black.jsx
@@ -0,0 +1,12 @@
+export default {
+   name: 'bloak',
+
+   render: function () {
+
+      return (
+         <div>
+            <h1>jsx 好像很好用的样子</h1>
+            <h2>它看起来很不错不是吗?</h2>
+         </div>
+      )
+
