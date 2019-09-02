add7a06e067cbebbd0671b1d75bfba2e69c64a1e
DBL <377014200@qq.com>
2019-09-02 14:54:03 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -4,29 +4,29 @@ import Router from 'vue-router';
 const routerConfig = {
    mode: 'history',
    // base: '/production/',
+   // routerBase 是在 config 文件中配置的环境变量
    base: routerBase || '/',
    linkExactActiveClass: 'active',
-   // 这个 routes 让我采坑了写成了 routers
    // component: lazy 必须要是函数返回, 在某些情况下,路径也是出现问题最多, 你很难发觉
    routes: [
       {
          path: '/',
-         component: () => import( /* webpackChunkName: "module/home" */ 'view/home/home' )
+         component: () => import( /* webpackChunkName: "module/home" */ 'view/home/Home' )
       },
       {
          path: '/sidebar',
          name: 'sidebar',
-         component: () => import( /* webpackChunkName: "module/sidebar" */ 'view/sidebar/sidebar' )
+         component: () => import( /* webpackChunkName: "module/sidebar" */ 'view/sidebar/Sidebar' )
       },
       {
          path: '/radius',
          name: 'radius',
-         component: () => import( /* webpackChunkName: "module/radius" */ 'view/radius/radius' )
+         component: () => import( /* webpackChunkName: "module/radius" */ 'view/radius/Radius' )
       },
       {
          path: '/black',
          name: 'black',
-         component: () => import( /* webpackChunkName: "module/jsx" */ 'view/jsx/black.jsx' )
+         component: () => import( /* webpackChunkName: "module/jsx" */ 'view/jsx/Black' )
       },
       {
          path: '*',
