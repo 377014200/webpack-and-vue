82ea3350f313beb3d257564cf5a5ba2da7bd79f8
DBL <377014200@qq.com>
2019-09-24 08:41:03 +0800

    添加 vuex 测试文件

---
+++ src/store/modules/home/index.js
@@ -0,0 +1,25 @@
+export default {
+   namespaced: true,
+   state: {
+      count: 0,
+   },
+   getter: {
+
+   },
+   mutations: {
+      increment ( state ) {
+
+         console.log( 'in home ' );
+         // 这里的 `state` 对象是模块的局部状态
+         state.count++;
+
+      }
+   },
+   actions: {
+      doubleCount ( state ) {
+
+         return state.count * 2;
+
+      }
+   }
+};
