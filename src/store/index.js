495f20d1c58adc5431241d56ac64fcc8da58e146
DBL <377014200@qq.com>
2019-09-24 08:38:39 +0800

    vuex 添加 hmr 的功能

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,3 @@
-
 import Vuex from 'vuex';
 import Vue from 'vue';
 
@@ -6,14 +5,63 @@ import state from './state';
 import mutations from './mutations';
 import actions from './actions';
 import getters from './getters';
+
+import home from './modules/home';
+import radius from './modules/radius';
+
 Vue.use( Vuex );
 
-export default new Vuex.Store( {
+/* eslint-disable no-shadow */
+const store = new Vuex.Store( {
    state,
    mutations,
    actions,
    getters,
    modules: {
+      home,
+      radius
+   },
+   plugins: [store => {
 
-   }
+      console.log( '查看 vuex plugin 选项' );
+      // 当 store 初始化后调用
+      store.subscribe( ( mutation ) => {
+      // 每次 mutation 之后调用
+      // mutation 的格式为 { type, payload }
+         console.log( mutation );
+      } );
+
+   }]
 } );
+
+
+// vuex 需要手动配置 hmr
+if ( module.hot ) {
+
+   module.hot.accept(
+      [
+         './getters',
+         './actions',
+         './mutations',
+         './modules/home',
+         './modules/radius'
+      ],
+      function () {
+
+         console.log( 'Accepting the updated in vuex!' );
+
+         store.hotUpdate( {
+            getters,
+            actions,
+            mutations,
+            modules: {
+               home,
+               radius
+            }
+         } );
+
+      }
+   );
+
+}
+export default store;
