b1ffdd42b5f10cb2c1fa2d44c1fcaae37c97a5fa
DBL <377014200@qq.com>
2019-10-07 22:34:43 +0800

    2019/10/07

--- bundle/plugin/MyWebpckPugin.js
+++ bundle/plugin/MyWebpckPugin.js
@@ -1,13 +1,21 @@
 class MyWebpackPlugin {
-   constructor (){
-      this.n = 0
+
+   constructor () {
+
+      this.n = 0;
+
    }
-   apply(compiler){
-      compiler.hooks.compile.tap('MyWebpackPlugin', (compilation,callback) => {
+   apply( compiler ) {
+
+      compiler.hooks.compile.tap( 'MyWebpackPlugin', () => {
+
          // 做一些异步的事情……
-         console.log('[ 欲上青天揽明月 ]: --------------------第'+ (++this.n) +'次编译------------------')
-      });
+         console.log( '[ 欲上青天揽明月 ]: --------------------第' + ++this.n + '次编译------------------' );
+
+      } );
+
    }
+
 }
 
-module.exports =  MyWebpackPlugin
+module.exports = MyWebpackPlugin;
